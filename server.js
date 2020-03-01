cc33b410f1154090bad9d01b115324c00b122e42
Igorth <igordiasth@gmail.com>
2020-03-01 17:59:44 +1300

    first route

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// toda vez que o usuario acessar a rota
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
 app.listen(3001);
