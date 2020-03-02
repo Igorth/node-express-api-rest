8fc3fdec582c61c61e239faebcb71e56f0dc2fd8
Igorth <igordiasth@gmail.com>
2020-03-02 17:11:56 +1300

    mongodb connect

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongose = require('mongoose');
 
+// Iniciando o APP
 const app = express();
 
+// Iniciando o Database
+mongose.connect('mongodb://localhost:27017/nodeapirest');
+
+// Primeira rota
 // toda vez que o usuario acessar a rota
 app.get('/', (req, res) => {
     res.send('Hello Roc');
