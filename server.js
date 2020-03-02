c2eb9a88f5f569eb150e47a6e0af1c4d3fe2a683
Igorth <igordiasth@gmail.com>
2020-03-02 17:23:31 +1300

    create model product, first register in the mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o APP
 const app = express();
@@ -7,10 +8,22 @@ const app = express();
 // Iniciando o Database
 mongose.connect('mongodb://localhost:27017/nodeapirest');
 
+
+requireDir('./src/models');
+
+const Product = mongose.model('Product');
+
+
+
 // Primeira rota
 // toda vez que o usuario acessar a rota
 app.get('/', (req, res) => {
-    res.send('Hello Roc');
+    Product.create({ 
+        title: 'React', 
+        description: 'Build native apps',
+        url: 'http://github.com/facebook/react-native'
+    })
+    return res.send('Hello Rock');
 });
 
 app.listen(3001);
